c1ca8ba895b64d4e0bbc6b1537e694fcbebaf6a4
Rodrigo-VilaNova <202200196@estudantes.ips.pt>
2023-06-19 00:44:12 +0100

    Remoção de código comentado no listElem.c

--- listElem.c
+++ listElem.c
@@ -3,16 +3,6 @@
 #include "listElem.h"
 
 void listElemPrint(ListElem elem) {
-	/*printf("\tID:\t%d\n",elem.id);
-	printf("\tCountry:\t%s\n",elem.countryCode);
-	printf("\tDate:\t%d/%d/%d\n",elem.date.day,elem.date.month,elem.date.year);
-	printf("\tTime:\t%d:%d:%d\n",elem.time.hour,elem.time.min,elem.time.sec);
-	printf("\tLatitude:\t%f\n",elem.latitude);
-	printf("\tLongitude:\t%f\n",elem.longitude);
-	printf("\tType:\t%s\n",elem.type);
-	printf("\tDepth:\t%f\n",elem.depth);
-    printf("\tMagnitude:\t%f\n",elem.magnitude);
-	printf("\tMagnitude Type:\t%s\n",elem.magnitudeType);*/
     printf("%*d",5,elem.id);
     printf("       %s",elem.countryCode);
     printf("      %0*d/%0*d/%0*d", 2, elem.date.day, 2, elem.date.month, 4, elem.date.year);
